2a39bfcb1a27e90c7e75d4b1723461740f7fdbee
kyishanz <minicloud118@gmail.com>
2021-12-30 12:59:13 -0800

    04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, v1, v2, v3, v4) {
+    let newArray = [];
+    array.forEach(value => {
+        let index = array.indexOf(value);
+        if (value !== v1 && value !== v2 && value !== v3 && value !== v4) {
+            newArray.push(value);
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
