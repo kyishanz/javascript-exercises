33092560ed15405b61dab6d0556e5d3a7eef1ff9
kyishanz <minicloud118@gmail.com>
2021-12-30 12:18:30 -0800

    02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, n) {
+    let repeated = "";
+    if (n < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < n; i++) {
+        repeated += string;
+    }
+    return repeated;
 };
 
 // Do not edit below this line
