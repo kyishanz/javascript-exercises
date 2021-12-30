8cfcf5d874c5dc5296a35fc4db9b99ad3b034175
kyishanz <minicloud118@gmail.com>
2021-12-30 12:22:50 -0800

    03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
 const reverseString = function(str) {
-
+    let reversed = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str.charAt(i);
+    }
+    return reversed;
 };
 
 // Do not edit below this line
