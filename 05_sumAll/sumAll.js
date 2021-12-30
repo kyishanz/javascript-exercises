1427960cd246ccd70b55e1a714571c7d3efa2e7d
kyishanz <minicloud118@gmail.com>
2021-12-30 14:56:48 -0800

    05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
+    if (typeof a != "number" || typeof b != "number" || a < 0 || b < 0) {
+        return "ERROR";
+    }
 
+    for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+        sum += i;
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
