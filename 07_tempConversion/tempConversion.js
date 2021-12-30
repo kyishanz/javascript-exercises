eb1807c130056e4cc5ca65e9d7719837e3b56cf1
kyishanz <minicloud118@gmail.com>
2021-12-30 15:28:55 -0800

    07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fT) {
+  let cT = (fT - 32) * 5.0 / 9;
+  cT = Math.round(cT * 10) / 10;
+  return cT;
 };
 
-const ctof = function() {
-
+const ctof = function(cT) {
+  let fT = (cT * 9.0/5) + 32;
+  fT = Math.round(fT * 10) / 10;
+  return fT;
 };
 
 // Do not edit below this line
