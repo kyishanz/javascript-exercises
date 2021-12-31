9d62f90e0e9278795bef2e32ab87117d4d304321
kyishanz <minicloud118@gmail.com>
2021-12-30 21:41:56 -0800

    10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS";
+    } else {
+        return recurse(Number(n));
+    }
 
 };
+
+function recurse (n) {
+    if (n <= 1) {
+        return n;
+    }
+    return recurse(n-2) + recurse(n-1);
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
